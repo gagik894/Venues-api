e64a46a60bc49d7c7129b650e0e004aabefa0426
gagik894 <gagiksargsyan894@gmail.com>
2025-12-20 16:07:58 +0100

    addet tests before deploying to server

--- booking/src/test/kotlin/app/venues/booking/TestApplication.kt
+++ booking/src/test/kotlin/app/venues/booking/TestApplication.kt
@@ -1,6 +1,12 @@
 package app.venues.booking
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = ["app.venues"])
+@EntityScan(basePackages = ["app.venues"])
 class TestApplication
